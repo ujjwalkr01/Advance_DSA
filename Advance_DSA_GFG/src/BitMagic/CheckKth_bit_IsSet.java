f97d058eeb31b4f4a58c714f32eb3d0accd93ec6
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-08-31 16:01:12 +0530

    update alternate method

--- Advance_DSA_GFG/src/BitMagic/CheckKth_bit_IsSet.java
+++ Advance_DSA_GFG/src/BitMagic/CheckKth_bit_IsSet.java
@@ -10,11 +10,29 @@ public class CheckKth_bit_IsSet {
 		int n = sc.nextInt();
 		int k = sc.nextInt();
 
+//		method-1 using left shift operator...
 		if (checkIF_kthBit_IsSet(n, k)) {
 			System.out.println("yes");
 		} else {
 			System.out.println("no");
 		}
+
+//		method-2 using right shift operator.....
+		if (checkIF_kthBit_IsSet_RightShift(n, k)) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+	}
+
+	private static boolean checkIF_kthBit_IsSet_RightShift(int n, int k) {
+
+		if ((n & (n >> (k - 1))) == 1) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 	private static boolean checkIF_kthBit_IsSet(int n, int k) {
