99588b6b651e86f0ae061a7d4ae12ccc57f207e1
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-09-01 16:27:20 +0530

    commit

---
+++ Advance_DSA_GFG/src/BitMagic/CheckPowerOf2.java
@@ -0,0 +1,28 @@
+package BitMagic;
+
+import java.util.Scanner;
+
+public class CheckPowerOf2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		// Using Brians Kernighan's algorithm
+		if (checkPowerOF_Two(num)) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+	}
+
+	private static boolean checkPowerOF_Two(int num) {
+
+		if (num == 0) {
+			return false;
+		}
+		return ((num & (num - 1)) == 0);
+	}
+
+}
